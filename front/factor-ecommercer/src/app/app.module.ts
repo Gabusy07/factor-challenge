8ce53934a145c561b4eb7fd138dc1171ac048758
gabriel remes <gabm.remes@gmail.com>
2024-05-01 14:39:28 -0300

    ADD: add initial components

--- front/factor-ecommercer/src/app/app.module.ts
+++ front/factor-ecommercer/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtomComponent } from './shared/buttom/buttom.component';
+import { InputComponent } from './shared/input/input.component';
+import { TableComponent } from './shared/table/table.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtomComponent,
+    InputComponent,
+    TableComponent,
+    HomeComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/factor-ecommercer/src/app/shared/buttom/buttom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttom',
+  templateUrl: './buttom.component.html',
+  styleUrls: ['./buttom.component.scss']
+})
+export class ButtomComponent {
+
+}
