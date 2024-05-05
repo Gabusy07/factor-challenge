e9e8f0eb9077c96839397cede467d31425bca6e5
gabriel remes <gabm.remes@gmail.com>
2024-05-05 19:34:31 -0300

    FIX: error data sql error

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -12,6 +12,16 @@ VALUES ('2024-07-20');
 
 
 INSERT INTO products (name, description, stock, price, image)
-VALUES ('Nombre del producto', 'Descripción del producto', 10, 99.99, 'URL_de_la_imagen');
+VALUES ('Iphone', 'smartphone', 16, 999.99, 'URL_de_la_imagen');
 
+INSERT INTO products (name, description, stock, price, image)
+VALUES ('smartwatch', 'reloj inteligente', 5, 55555550.0, 'URL_de_la_imagen');
+
+INSERT INTO products (name, description, stock, price, image)
+VALUES ('camisa', 'camisa blanca ...', 5, 2.33, 'URL_de_la_imagen');
+
+INSERT INTO users (username, password, type)
+VALUES
+  ('common02@email.com', '$2a$10$Cf4n523B6JbOFOx1ic1GxeTTWvgaGHc2RZ8hLEvRKVPtB4KvUji2K', 'USER_COMMON'),
+  ('vip02@email.com', '$2a$10$spip6c9hUvIEKITkvKpCq.lrXg3qBN2JY9EGkwTvQsHoKoWNsFZ56', 'USER_VIP') ON CONFLICT (username) DO NOTHING;
 
