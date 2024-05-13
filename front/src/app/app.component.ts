3da0ae8c12d8deb888b87a3acc2edaea3dc5391d
gabriel-remes <gabm.remes@gmail.com>
2024-05-12 21:08:43 -0300

    CHG: app component import path

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment as env } from '../environments/environment.dev';
+import { environment as env } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
